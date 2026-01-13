b3c28abfa88dc7c485f539eacbe6f632a1daa8e9
saniya.patel <786saniya.patel@gmail.com>
2026-01-13 16:42:15 +0530

    Initial commit

---
+++ src/main/java/Login.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+}

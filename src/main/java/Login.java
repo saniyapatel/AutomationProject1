6c532d0c486150278cf80eb3626957d5ab339397
saniya.patel <786saniya.patel@gmail.com>
2026-01-14 13:11:37 +0530

    1 added few code in Login file

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -5,5 +5,6 @@ public class Login {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        // adding new changes here
     }
 }

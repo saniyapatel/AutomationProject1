0c5c6f61078901a47022c7b959c9bf4be0c6d25f
saniyapatel <50191445+saniyapatel@users.noreply.github.com>
2026-06-23 14:46:47 +0530

    Feature/school new (#7)
    * removed testing sout
    
    * testing 123
    
    * added SchoolNew class in SchoolNew branch

---
+++ src/main/java/SchoolNew.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class SchoolNew {
+
+        public static void main(String[] args){
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("https://www.w3schools.com/html/html_tables.asp");
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+            By tableBody = By.xpath("//tbody//tr//th[text()='Company']");
+            wait.until(ExpectedConditions.visibilityOfElementLocated(tableBody));
+        }
+    }
+
+

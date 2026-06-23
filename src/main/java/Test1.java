d9051bee6fc900eae8dd15042a861731042ee801
saniya.patel <786saniya.patel@gmail.com>
2026-06-23 15:37:23 +0530

    addec Test1 class

---
+++ src/main/java/Test1.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Test1 {
+    public static void main(String[] args){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.w3schools.com/html/html_tables.asp");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        By tableBody = By.xpath("//tbody//tr//th[text()='Company']");
+
+        String expectedResult = "Common";
+        String actualResult = wait.until(ExpectedConditions.visibilityOfElementLocated(tableBody)).getText();
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+}
